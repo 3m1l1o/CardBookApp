50fc17122e9ab151a02b6c315dc8888d3f10c1a8
emilioTM <emiliogallo80@gmail.com>
2024-07-31 15:53:07 -0300

    app done

---
+++ src/components/BookList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BookShow } from "./BookShow";
+export const BookList = ({ books, onDelete, onEdit }) => {
+
+  const renderBooks = books.map((book) => {
+    return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
+  });
+
+  return <div className="book-list">{renderBooks}</div>;
+};
